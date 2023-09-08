2c3b1fac9f63669fa529b6b972d9ce49e936687d
Pedro Dalvy <pedrodalvy@outlook.com>
2023-09-07 22:49:26 -0300

    ci: add github workflow config

--- k6/scripts/hello-world.k6.js
+++ k6/scripts/hello-world.k6.js
@@ -1,4 +1,4 @@
-import { check } from 'k6';
+import { check, sleep } from 'k6';
 import http from 'k6/http';
 
 export const options = {
@@ -7,6 +7,10 @@ export const options = {
     { duration: '20s', target: 100 },
     { duration: '20s', target: 0 },
   ],
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(95)<5'], // 95 percent of response times must be below 5ms
+  },
 };
 
 export default () => {
@@ -15,4 +19,6 @@ export default () => {
   check(response, {
     'Verify response text': (r) => r.body.includes('Hello World!'),
   });
+
+  sleep(1);
 };
